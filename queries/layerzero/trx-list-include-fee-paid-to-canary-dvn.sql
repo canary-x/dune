c8880d358e605705abe9b222bb7793d7a8688e9d
Zeinab Alipanahloo <zeinab@Zeinabs-MacBook-Air.local>
2026-01-19 23:28:11 -0500

    add trx-list-include-fee-paid-to-canary-dvn query

---
+++ queries/layerzero/trx-list-include-fee-paid-to-canary-dvn.sql
@@ -0,0 +1,20 @@
+select
+    chain
+    , evt_block_date
+    , evt_tx_hash
+    , DVN
+    , fee
+from layerzero_multichain.senduln302_evt_dvnfeepaid
+    cross join unnest (zip(concat(requiredDVNs, optionalDVNs), fees)) as t (DVN, fee)
+where (
+        chain = 'base'
+   and DVN = 0x554833698ae0fb22ecc90b01222903fd62ca4b47
+)
+   or (
+        chain = 'ethereum'
+   and DVN = 0xa4fe5a5b9a846458a70cd0748228aed3bf65c2cd
+)
+   or (
+        chain = 'arbitrum'
+   and DVN = 0xf2e380c90e6c09721297526dbc74f870e114dfcb
+)
