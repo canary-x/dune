6476197d7325235f6fb7db4b7bd6a529f1c5bb2d
Zeinab Alipanahloo <zeinab@Zeinabs-MacBook-Air.local>
2026-02-04 11:44:17 -0500

    add morpho query

---
+++ queries/layerzero/tokens/morpho-volume-secured-by-canary-dvn.sql
@@ -0,0 +1,34 @@
+-- queryid: #6639658
+with filtered_transfers as (
+    select
+        t.tx_hash
+        , t.block_date
+        , t.amount
+        , t.contract_address
+        , t.blockchain
+        from tokens.transfers t
+            inner join dune.zeinab_team_8277.result_transaction_list_include_fee_paide_to_l0_canary_dvn_wbtc_canary_dvn l on l.evt_tx_hash = t.tx_hash
+        where t.block_date >= current_date - interval '6' month
+   and t.blockchain in ('ethereum', 'arbitrum')
+   and t.contract_address in (
+                0x58D97B57BB95320F9a05dC918Aef65434969c2B2
+        , 0x40BD670A58238e6E230c430BBb5cE6ec0d40df48
+)
+)
+        , filtered_prices as (
+        select
+        contract_address
+        , blockchain
+        , timestamp
+        , price
+        from dune.saharap.result_filter_prices_day
+        where timestamp >= current_date - interval '6' month
+   and blockchain in ('arbitrum')
+   and contract_address in (0x40BD670A58238e6E230c430BBb5cE6ec0d40df48)
+)
+select
+    t.block_date
+    , t.amount as volume
+    , t.amount * p.price as volume_USD
+from filtered_transfers t
+    left join filtered_prices p on p.timestamp = t.block_date
