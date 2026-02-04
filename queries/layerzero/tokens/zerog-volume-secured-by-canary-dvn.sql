54f6978046f2f134d4e189e42fd3d46b4290bb77
Zeinab Alipanahloo <zeinab@Zeinabs-MacBook-Air.local>
2026-02-04 11:41:54 -0500

    add zerog query

---
+++ queries/layerzero/tokens/zerog-volume-secured-by-canary-dvn.sql
@@ -0,0 +1,33 @@
+-- queryid: #6633212
+with filtered_transfers as (
+    select
+        t.tx_hash
+        , t.block_date
+        , t.amount
+        , t.contract_address
+        , t.blockchain
+        from tokens.transfers t
+            inner join dune.zeinab_team_8277.result_transaction_list_include_fee_paide_to_l0_canary_dvn_wbtc_canary_dvn l on l.evt_tx_hash = t.tx_hash
+        where t.block_date >= current_date - interval '3' month
+   and t.blockchain in ('ethereum')
+   and t.contract_address in (0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf)
+)
+        , filtered_prices as (
+        select
+        contract_address
+        , blockchain
+        , timestamp
+        , price
+        from dune.saharap.result_filter_prices_day
+        where timestamp >= current_date - interval '3' month
+   and blockchain in ('ethereum')
+   and contract_address in (0xcbB7C0000aB88B473b1f5aFd9ef808440eed33Bf)
+)
+select
+    t.block_date
+    , t.amount as volume
+    , t.amount * p.price as volume_USD
+from filtered_transfers t
+    left join filtered_prices p on p.contract_address = t.contract_address
+   and p.blockchain = t.blockchain
+   and p.timestamp = t.block_date;
